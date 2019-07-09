66dfa118375a8b30ac14f7361dd385e083eb4e81
Yevgeniy <zleonenkov@gmail.com>
2019-07-09 14:57:40 +0300

    Updates

---
+++ precache-manifest.5a9c6b7da33cfe37cc97f49e93d33003.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c3a1d547fba041cab493a963e638c7",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "1099ef9d12c942293371",
+    "url": "/react_dynamic-list-of-posts/static/css/main.a31e3f04.chunk.css"
+  },
+  {
+    "revision": "509cf8d9502ff8912294",
+    "url": "/react_dynamic-list-of-posts/static/js/2.cc6de668.chunk.js"
+  },
+  {
+    "revision": "1099ef9d12c942293371",
+    "url": "/react_dynamic-list-of-posts/static/js/main.77443a4b.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
